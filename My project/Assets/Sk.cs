25ce140ab1e07ef153c88c649c59e17225882926
Aliaksei <gotroc59@gmail.com>
2022-03-13 18:07:24 +0100

    Wgrywanie plików z Unity do repozytorium

---
+++ My project/Assets/Sk.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sk : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 movement;
+    public float speed = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.z = Input.GetAxis("Vertical");
+
+        if (controller.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                movement.y = 1f;
+            }
+
+        }
+        else { movement.y -= 0.01f; }
+
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        controller.Move(movement * Time.fixedDeltaTime * speed);
+
+
+    }
+}
---
+++ My project/Assets/WinArea.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class WinArea : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        string index = "Ers";
+
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(index);
+        }
+    }
+}
